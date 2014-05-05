6f682dfd5825a19665acb116f34ee153a8477d3e
alex greenwood <alexander.greenwood@gmail.com>
2014-05-04 19:46:08 -0500

    updated with summaries

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,17 +3,18 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 42.0538387
-    @longitude = -87.67721
-    your_api_key = ""
+    @latitude = 37.4219988
+    @longitude = -122.083954
+    your_api_key = "7aed3b73c48cca63671473e41fac875d"
 
-    # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @temperature = ?
-    # @minutely_summary = ?
-    # @hourly_summary = ?
-    # @daily_summary = ?
+
+    url = "https://api.forecast.io/forecast/7aed3b73c48cca63671473e41fac875d/37.421998,-122.083954"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @temperature = parsed_data["currently"]["temperature"]
+    @minutely_summary = parsed_data["minutely"]["summary"]
+    @hourly_summary = parsed_data["hourly"]["summary"]
+    @daily_summary = parsed_data["daily"]["summary"]
+
   end
 end
