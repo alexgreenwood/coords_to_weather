3883fea1c42551c03c776d5633942ee376ba745b
alex greenwood <alexander.greenwood@gmail.com>
2014-05-04 21:10:10 -0500

    got /weather to show up when no address input
    yay!

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -4,8 +4,8 @@ require 'json'
 class CoordsController < ApplicationController
 
 def fetch_weather
-    @address = params[:address]
 
+if @address = params[:address]
 
     @url_safe_address = URI.encode(@address)
 
@@ -25,5 +25,25 @@ def fetch_weather
     @hourly_summary = parsed_data["hourly"]["summary"]
     @daily_summary = parsed_data["daily"]["summary"]
 
+else
+
+    @address != params[:address]
+
+    @latitude = 37.8267
+    @longitude = -122.423
+
+    your_api_key = "7aed3b73c48cca63671473e41fac875d"
+
+    url = "https://api.forecast.io/forecast/7aed3b73c48cca63671473e41fac875d/37.8267,-122.423"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @temperature = parsed_data["currently"]["temperature"]
+    @minutely_summary = parsed_data["minutely"]["summary"]
+    @hourly_summary = parsed_data["hourly"]["summary"]
+    @daily_summary = parsed_data["daily"]["summary"]
+
+
   end
 end
+end
+
