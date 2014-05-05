9c53b3e03bcd39a9e882973daa0b67daae1f104e
alex greenwood <alexander.greenwood@gmail.com>
2014-05-04 19:50:52 -0500

    weather outputs change per lat & lng variables

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,12 +3,12 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 37.4219988
-    @longitude = -122.083954
+    @latitude = 33.4219988
+    @longitude = -120.083954
     your_api_key = "7aed3b73c48cca63671473e41fac875d"
 
 
-    url = "https://api.forecast.io/forecast/7aed3b73c48cca63671473e41fac875d/37.421998,-122.083954"
+    url = "https://api.forecast.io/forecast/7aed3b73c48cca63671473e41fac875d/#{@latitude},#{@longitude}"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
     @temperature = parsed_data["currently"]["temperature"]
